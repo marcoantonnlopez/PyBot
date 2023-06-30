612a54bcb1ce6f01cb2dd07f58093273e3ecca4f
marcoantonnlopez <A305350@alumnos.uaslp.mx>
2023-06-29 22:00:02 -0600

    landing v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,50 @@ import Layout from './layout';
 import anime from 'animejs';
 
 export default function Home() {
+
   return (
     <main>
       <div className="h-full">
         <div className="container pt-24 md:pt-36 mx-8 flex flex-wrap flex-col md:flex-row items-center">
           <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-            <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left mb-8 ">
-              <span className="bg-clip-text text-transparent bg-white">
-                Py
-              </span>
-              Bot
-            </h1>
-            <p className="leading-normal text-base md:text-2xl text-center md:text-left mb-8 ">
+
+            <img
+              src="logo.svg" // Reemplaza con la URL de la imagen que deseas mostrar
+              alt="Logo"
+              className="mb-8 w-65 float-left "
+              width={200}
+              height={300}
+            />
+
+            <p className="leading-normal text-base md:text-4xl text-center md:text-left mb-2 font-bold">
               Unlock the Power of Python with PyBot
             </p>
-            <h6 className="text-white opacity-75 font-bold leading-tight text-center md:text-left mb-8 ">
+            <h6 className="text-white opacity-75 leading-tight text-center md:text-left mb-8 ">
               Powered by ChatGPT
             </h6>
 
             <div className="flex items-center justify-between pt-4">
-              <Link href="/chat">
-                <button
-                  className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-blue-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded-full focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+              {/* <Link href="/chat"> */}
+                {/* <button
+                  className="bg-primary hover:bg-gradient-to-r hover:from-primary hover:via-secondary hover:to-primary text-white font-bold py-2 px-4 rounded-md focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                   type="button"
+                  onClick={handleStartClick}
                 >
                   Start
-                </button>
+                </button> */}
+              {/* </Link> */}
+
+            <Link href="/chat" passHref>
+                <button className="bg-primary hover:bg-gradient-to-r hover:from-primary hover:via-secondary hover:to-primary text-white font-bold py-2 px-4 rounded-md focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
+                Start
+               </button>
               </Link>
             </div>
           </div>
 
           <div className="w-full xl:w-3/5 p-12 overflow-hidden">
             <img
-              src="macbook.png" // Reemplaza con la URL de la imagen que deseas mostrar
+              src="macbook.svg" // Reemplaza con la URL de la imagen que deseas mostrar
               alt="Descripción de la imagen"
               className="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6"
               width={500}
